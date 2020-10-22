98cda32657b32b833c4664b9d4302716f3c10769
Zach Bayard <zbayard@gmail.com>
2020-10-22 15:45:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  statement = "It's Dangerous To Go Alone! Take This"
+  puts statement
+end
